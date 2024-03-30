c7723c290f5b399056f0502ea023297ebd87859d
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2024-03-30 13:48:11 +0530

    updates

--- practice/leetcode1.js
+++ practice/leetcode1.js
@@ -1,15 +1,54 @@
+// var nums = [2, 7, 11, 15];
+// var target = 9;
+
+// function TwoSumNumbers(nums, target) {
+//     for (var i = 0; i < nums.length - 1; i++) {
+//         for (var j = i + 1; j < nums.length; j++) {
+//             if(nums[i] + nums[j] == target) {
+//                 console.log(i,j)
+//             }
+//         }
+//     }
+// }
+
+
+// TwoSumNumbers(nums, target)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var nums = [2, 7, 11, 15];
 var target = 9;
 
 function TwoSumNumbers(nums, target) {
-    for (var i = 0; i < nums.length - 1; i++) {
-        for (var j = i + 1; j < nums.length; j++) {
+    for(var i = 0; i < nums.length - 1; i++) {
+        for(var j = 0; j < nums.length; j++) {
             if(nums[i] + nums[j] == target) {
-                console.log(i,j)
+                console.log(nums[i], nums[j])
             }
         }
     }
 }
-
 
 TwoSumNumbers(nums, target)
